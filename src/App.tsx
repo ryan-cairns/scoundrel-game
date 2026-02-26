d7e7e5536eaeff659fd03e9deb496821a0516ae9
Ryan Cairns <ryan9cairns@gmail.com>
2026-02-26 22:03:19 +0000

    Changed information displayed on home page to show starting room

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { shuffle, buildDeck, dealRoom } from "./game/Deck";
+import type { Card, Deck } from "./types";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initDeck = shuffle(buildDeck());
+  const { room: initRoom, remaining: initRemaining } = dealRoom(initDeck);
+
+  const [deck, setDeck] = useState<Deck>(initRemaining);
+  const [room, setRoom] = useState<Card[]>(initRoom);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Cards in dungeon: {deck.cards.length}</p>
+      <p>Cards in room:</p>
+      <ul>
+        {room.map((card, index) => (
+          <li key={index}>
+            {card.value} of {card.suit}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
