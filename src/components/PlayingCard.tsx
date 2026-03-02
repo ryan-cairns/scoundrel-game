db13517b05d3df9e5fc3de7ca416bd0f2c967a50
Ryan Cairns <ryan9cairns@gmail.com>
2026-03-02 15:40:42 +0000

    added playing card styles

--- src/components/PlayingCard.tsx
+++ src/components/PlayingCard.tsx
@@ -1,22 +1,24 @@
 import type { Card } from "../types";
+import "../styles/PlayingCard.css";
 
 interface Props {
   card: Card;
 }
 
 function PlayingCard({ card }: Props) {
+  const isRed = card.colour === "Red";
   return (
-    <div className="card">
-      <div className="card-corner top-left">
-        <span className="card-value">{card.value}</span>
-        <span className="card-symbol">{card.symbol}</span>
+    <div className={`playing-card ${isRed ? "red" : "black"}`}>
+      <div className="playing-card-corner top-left">
+        <span className="playing-card-value">{card.value}</span>
+        <span className="playing-card-symbol">{card.symbol}</span>
       </div>
-      <div className="card-center">
-        <span className="card-symbol">{card.symbol}</span>
+      <div className="playing-card-centre">
+        <span className="playing-card-symbol centre">{card.symbol}</span>
       </div>
-      <div className="card-corner bottom-right">
-        <span className="card-value">{card.value}</span>
-        <span className="card-symbol">{card.symbol}</span>
+      <div className="playing-card-corner bottom-right">
+        <span className="playing-card-value">{card.value}</span>
+        <span className="playing-card-symbol">{card.symbol}</span>
       </div>
     </div>
   );
